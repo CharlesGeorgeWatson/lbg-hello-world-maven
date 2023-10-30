05909a28b4b3ca0e83724cb79558f8ca57a624c6
Charles Watson <charlie.g.watson@gmail.com>
2023-10-30 15:58:32 +0000

    added cup song

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("I GOT MY TICKET FOR THE LOOOOONG WAY ROUNDDDDD"));
         System.out.println(sayGoodbye());
     }
 
